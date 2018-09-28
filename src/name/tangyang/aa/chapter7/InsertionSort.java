a788dad219bf67afb9d89c07fd23f43c645b3f3d
TangYang798 <tangyang798@gmail.com>
2018-09-28 23:37:47 +0800

    InsertionSort

---
+++ src/name/tangyang/aa/chapter7/InsertionSort.java
@@ -0,0 +1,18 @@
+package name.tangyang.aa.chapter7;
+
+/**
+ * InsertionSort p187
+ * @param <AnyType[]>
+ */
+public class InsertionSort {
+    public static <AnyType extends Comparable<? super AnyType>> void insertionSort(AnyType[] a) {
+        int j;
+        for (int p = 1; p < a.length; p++) {
+            AnyType tmp = a[p];
+            for (j = p; j > 0 && tmp.compareTo(a[j - 1]) < 0; j--) {
+                a[j] = a[j - 1];
+            }
+            a[j] = tmp;
+        }
+    }
+}
