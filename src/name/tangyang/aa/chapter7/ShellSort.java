774e961d40e352514c59f55989e1f7ef8f9adbe9
TangYang798 <tangyang798@gmail.com>
2018-09-29 23:12:24 +0800

    ShellSort

---
+++ src/name/tangyang/aa/chapter7/ShellSort.java
@@ -0,0 +1,20 @@
+package name.tangyang.aa.chapter7;
+
+/**
+ * ShellSort p189
+ * @param a
+ */
+public class ShellSort {
+    public static <AnyType extends Comparable<? super AnyType>> void shellSort(AnyType[] a) {
+        int j;
+        for (int gap = a.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < a.length; i++) {
+                AnyType tmp = a[i];
+                for (j = i; j >= gap && tmp.compareTo(a[j - gap]) < 0; j -= gap) {
+                    a[j] = a[j - gap];
+                }
+                a[j] = tmp;
+            }
+        }
+    }
+}
