8654f2b4d06076da8ce16ea031dda85fb98d7d7a
TangYang798 <tangyang798@gmail.com>
2018-10-07 20:26:42 +0800

    QuickSort

---
+++ src/name/tangyang/aa/chapter7sort/QuickSort.java
@@ -0,0 +1,49 @@
+package name.tangyang.aa.chapter7sort;
+
+/**
+ * QuickSort p204
+ *
+ */
+public class QuickSort {
+    private static final int CUTOFF = 20;
+
+    public static <AnyType extends Comparable<? super AnyType>> void quickSort(AnyType[] a) {
+        quickSort(a, 0, a.length - 1);
+    }
+
+    private static <AnyType extends Comparable<? super AnyType>> void quickSort(AnyType[] a, int left, int right) {
+        if (left + CUTOFF < right) {
+            AnyType pivot = median(a, left, right);
+            int i = left, j = right;
+            for (;;) {
+                while (a[++i].compareTo(pivot) < 0){}
+                while (a[++i].compareTo(pivot) > 0){}
+                if (i < j) {
+                    HeapSort.swapReferefces(a, i, j);
+                } else {
+                    break;
+                }
+            }
+            HeapSort.swapReferefces(a, i, right - 1);
+            quickSort(a, left, i - 1);
+            quickSort(a, i + 1, right);
+        } else {
+            InsertionSort.insertionSort(a);
+        }
+    }
+
+    public static <AnyType extends Comparable<? super AnyType>> AnyType median(AnyType[] a, int left, int right) {
+        int center = (left + right) / 2;
+        if (a[center].compareTo(a[left]) < 0) {
+            HeapSort.swapReferefces(a, left, center);
+        }
+        if (a[right].compareTo(a[left]) < 0) {
+            HeapSort.swapReferefces(a, right, left);
+        }
+        if (a[right].compareTo(a[center]) < 0) {
+            HeapSort.swapReferefces(a, right, center);
+        }
+        HeapSort.swapReferefces(a, center, right - 1);
+        return a[right - 1];
+    }
+}
